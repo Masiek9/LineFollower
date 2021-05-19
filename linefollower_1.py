30a80a209dc2b071d159b8f11384f04a9d142b46
Masiek9 <61582624+Masiek9@users.noreply.github.com>
2021-05-19 10:40:48 +0200

    Update linefollower_1.py

--- linefollower_1.py
+++ linefollower_1.py
@@ -1,34 +1,72 @@
-
 # from controller import Camera
 from controller import Robot
+from controller import Camera
+from controller import  DistanceSensor, Motor
+
 
 def run_robot(robot):
+    camera = Camera('camera')
+    camera.enable(100)
 
     time_step = 32
-    max_speed = 6.28
+    
+    max_speed = 3
+    
     
     #motors
-    left_motor = robot.getMotor('left wheel motor')
-    right_motor = robot.getMotor('right wheel motor')
+    left_motor = robot.getDevice('left wheel motor')
+    right_motor = robot.getDevice('right wheel motor')
     left_motor.setPosition(float('inf'))
     right_motor.setPosition(float('inf'))
     left_motor.setVelocity(0.0)
     right_motor.setVelocity(0.0)
     
+    #irsensors
+    left_ir = robot.getDevice('IRL')
+    left_ir.enable(time_step)
+    
+    right_ir = robot.getDevice('IRR')
+    right_ir.enable(time_step)
+       
+       
     # Step simulation
     while robot.step(time_step) != -1:
-        left_speed = max_speed
-        right_speed = max_speed
+    
+        left_ir_value = left_ir.getValue()
+        right_ir_value = right_ir.getValue()
+        
+        print("left: {}  right: {} ".format( left_ir_value, right_ir_value))
+        
+        left_speed = max_speed * 0.25
+        right_speed = max_speed * 0.25
+        
+        if (left_ir_value > right_ir_value) and (6 < left_ir_value < 15):
+            print("Go left")
+            left_speed = -max_speed * 0.25
+        elif (right_ir_value > left_ir_value) and (6 < right_ir_value < 15):
+            print("Go right")
+            right_speed = -max_speed * 0.25
         
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
     
     
 if __name__ == "__main__":
+    
     my_robot = Robot()
     run_robot(my_robot)
     
     
+
+ 
     
+
+
+
+    
+    
+    
+
+
 
 
